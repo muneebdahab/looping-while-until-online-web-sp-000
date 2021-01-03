ab5a282bce5ecb4ecb4669b230485763ea44d7e8
Muneeb Dahab <muneeb.dahab@outlook.com>
2021-01-03 10:48:44 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,8 +2,9 @@ def using_while
   levitation_force = 6
 	
   while levitation_force < 10
-  puts using_while "Wingardium Leviosa"
   levitation_force += 1
+  puts using_while "Wingardium Leviosa"
+
 end
 end
 
